297e2e97dc449dafd87ae0845c4e5a67e4df3342
ARaus97 <rauschandrew97@yahoo.com>
2022-02-14 00:09:41 -0800

    Add project files.

---
+++ Database/Form1.Designer.vb
@@ -0,0 +1,421 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.Label1 = New System.Windows.Forms.Label()
+        Me.Label2 = New System.Windows.Forms.Label()
+        Me.Label3 = New System.Windows.Forms.Label()
+        Me.Label4 = New System.Windows.Forms.Label()
+        Me.Label5 = New System.Windows.Forms.Label()
+        Me.Label6 = New System.Windows.Forms.Label()
+        Me.Label7 = New System.Windows.Forms.Label()
+        Me.Label8 = New System.Windows.Forms.Label()
+        Me.Label9 = New System.Windows.Forms.Label()
+        Me.Label10 = New System.Windows.Forms.Label()
+        Me.Label11 = New System.Windows.Forms.Label()
+        Me.Label12 = New System.Windows.Forms.Label()
+        Me.Label13 = New System.Windows.Forms.Label()
+        Me.txtEmpID = New System.Windows.Forms.TextBox()
+        Me.txtFirstName = New System.Windows.Forms.TextBox()
+        Me.txtLastName = New System.Windows.Forms.TextBox()
+        Me.txtAddress1 = New System.Windows.Forms.TextBox()
+        Me.txtAddress2 = New System.Windows.Forms.TextBox()
+        Me.txtCity = New System.Windows.Forms.TextBox()
+        Me.txtState = New System.Windows.Forms.TextBox()
+        Me.txtZIP = New System.Windows.Forms.TextBox()
+        Me.txtPhone = New System.Windows.Forms.TextBox()
+        Me.txtDOB = New System.Windows.Forms.TextBox()
+        Me.txtDOH = New System.Windows.Forms.TextBox()
+        Me.txtDept = New System.Windows.Forms.TextBox()
+        Me.txtJob = New System.Windows.Forms.TextBox()
+        Me.Label14 = New System.Windows.Forms.Label()
+        Me.CheckBox1 = New System.Windows.Forms.CheckBox()
+        Me.btnFirst = New System.Windows.Forms.Button()
+        Me.btnPrior = New System.Windows.Forms.Button()
+        Me.btnNext = New System.Windows.Forms.Button()
+        Me.btnLast = New System.Windows.Forms.Button()
+        Me.btnClose = New System.Windows.Forms.Button()
+        Me.lblPosition = New System.Windows.Forms.Label()
+        Me.SuspendLayout()
+        '
+        'Label1
+        '
+        Me.Label1.AutoSize = True
+        Me.Label1.Location = New System.Drawing.Point(83, 35)
+        Me.Label1.Name = "Label1"
+        Me.Label1.Size = New System.Drawing.Size(67, 13)
+        Me.Label1.TabIndex = 0
+        Me.Label1.Text = "Employee ID"
+        '
+        'Label2
+        '
+        Me.Label2.AutoSize = True
+        Me.Label2.Location = New System.Drawing.Point(83, 60)
+        Me.Label2.Name = "Label2"
+        Me.Label2.Size = New System.Drawing.Size(57, 13)
+        Me.Label2.TabIndex = 1
+        Me.Label2.Text = "First Name"
+        '
+        'Label3
+        '
+        Me.Label3.AutoSize = True
+        Me.Label3.Location = New System.Drawing.Point(83, 85)
+        Me.Label3.Name = "Label3"
+        Me.Label3.Size = New System.Drawing.Size(58, 13)
+        Me.Label3.TabIndex = 2
+        Me.Label3.Text = "Last Name"
+        '
+        'Label4
+        '
+        Me.Label4.AutoSize = True
+        Me.Label4.Location = New System.Drawing.Point(83, 110)
+        Me.Label4.Name = "Label4"
+        Me.Label4.Size = New System.Drawing.Size(54, 13)
+        Me.Label4.TabIndex = 3
+        Me.Label4.Text = "Address 1"
+        '
+        'Label5
+        '
+        Me.Label5.AutoSize = True
+        Me.Label5.Location = New System.Drawing.Point(83, 135)
+        Me.Label5.Name = "Label5"
+        Me.Label5.Size = New System.Drawing.Size(54, 13)
+        Me.Label5.TabIndex = 4
+        Me.Label5.Text = "Address 2"
+        '
+        'Label6
+        '
+        Me.Label6.AutoSize = True
+        Me.Label6.Location = New System.Drawing.Point(83, 160)
+        Me.Label6.Name = "Label6"
+        Me.Label6.Size = New System.Drawing.Size(24, 13)
+        Me.Label6.TabIndex = 5
+        Me.Label6.Text = "City"
+        '
+        'Label7
+        '
+        Me.Label7.AutoSize = True
+        Me.Label7.Location = New System.Drawing.Point(83, 185)
+        Me.Label7.Name = "Label7"
+        Me.Label7.Size = New System.Drawing.Size(32, 13)
+        Me.Label7.TabIndex = 6
+        Me.Label7.Text = "State"
+        '
+        'Label8
+        '
+        Me.Label8.AutoSize = True
+        Me.Label8.Location = New System.Drawing.Point(83, 210)
+        Me.Label8.Name = "Label8"
+        Me.Label8.Size = New System.Drawing.Size(24, 13)
+        Me.Label8.TabIndex = 7
+        Me.Label8.Text = "ZIP"
+        '
+        'Label9
+        '
+        Me.Label9.AutoSize = True
+        Me.Label9.Location = New System.Drawing.Point(83, 235)
+        Me.Label9.Name = "Label9"
+        Me.Label9.Size = New System.Drawing.Size(78, 13)
+        Me.Label9.TabIndex = 8
+        Me.Label9.Text = "Phone Number"
+        '
+        'Label10
+        '
+        Me.Label10.AutoSize = True
+        Me.Label10.Location = New System.Drawing.Point(83, 260)
+        Me.Label10.Name = "Label10"
+        Me.Label10.Size = New System.Drawing.Size(66, 13)
+        Me.Label10.TabIndex = 9
+        Me.Label10.Text = "Date of Birth"
+        '
+        'Label11
+        '
+        Me.Label11.AutoSize = True
+        Me.Label11.Location = New System.Drawing.Point(83, 285)
+        Me.Label11.Name = "Label11"
+        Me.Label11.Size = New System.Drawing.Size(64, 13)
+        Me.Label11.TabIndex = 10
+        Me.Label11.Text = "Date of Hire"
+        '
+        'Label12
+        '
+        Me.Label12.AutoSize = True
+        Me.Label12.Location = New System.Drawing.Point(83, 310)
+        Me.Label12.Name = "Label12"
+        Me.Label12.Size = New System.Drawing.Size(30, 13)
+        Me.Label12.TabIndex = 11
+        Me.Label12.Text = "Dept"
+        '
+        'Label13
+        '
+        Me.Label13.AutoSize = True
+        Me.Label13.Location = New System.Drawing.Point(83, 335)
+        Me.Label13.Name = "Label13"
+        Me.Label13.Size = New System.Drawing.Size(24, 13)
+        Me.Label13.TabIndex = 12
+        Me.Label13.Text = "Job"
+        '
+        'txtEmpID
+        '
+        Me.txtEmpID.Location = New System.Drawing.Point(206, 32)
+        Me.txtEmpID.Name = "txtEmpID"
+        Me.txtEmpID.Size = New System.Drawing.Size(144, 20)
+        Me.txtEmpID.TabIndex = 13
+        '
+        'txtFirstName
+        '
+        Me.txtFirstName.Location = New System.Drawing.Point(206, 57)
+        Me.txtFirstName.Name = "txtFirstName"
+        Me.txtFirstName.Size = New System.Drawing.Size(144, 20)
+        Me.txtFirstName.TabIndex = 14
+        '
+        'txtLastName
+        '
+        Me.txtLastName.Location = New System.Drawing.Point(206, 82)
+        Me.txtLastName.Name = "txtLastName"
+        Me.txtLastName.Size = New System.Drawing.Size(144, 20)
+        Me.txtLastName.TabIndex = 15
+        '
+        'txtAddress1
+        '
+        Me.txtAddress1.Location = New System.Drawing.Point(206, 107)
+        Me.txtAddress1.Name = "txtAddress1"
+        Me.txtAddress1.Size = New System.Drawing.Size(144, 20)
+        Me.txtAddress1.TabIndex = 16
+        '
+        'txtAddress2
+        '
+        Me.txtAddress2.Location = New System.Drawing.Point(206, 133)
+        Me.txtAddress2.Name = "txtAddress2"
+        Me.txtAddress2.Size = New System.Drawing.Size(144, 20)
+        Me.txtAddress2.TabIndex = 17
+        '
+        'txtCity
+        '
+        Me.txtCity.Location = New System.Drawing.Point(206, 157)
+        Me.txtCity.Name = "txtCity"
+        Me.txtCity.Size = New System.Drawing.Size(144, 20)
+        Me.txtCity.TabIndex = 18
+        '
+        'txtState
+        '
+        Me.txtState.Location = New System.Drawing.Point(206, 182)
+        Me.txtState.Name = "txtState"
+        Me.txtState.Size = New System.Drawing.Size(144, 20)
+        Me.txtState.TabIndex = 19
+        '
+        'txtZIP
+        '
+        Me.txtZIP.Location = New System.Drawing.Point(206, 207)
+        Me.txtZIP.Name = "txtZIP"
+        Me.txtZIP.Size = New System.Drawing.Size(144, 20)
+        Me.txtZIP.TabIndex = 20
+        '
+        'txtPhone
+        '
+        Me.txtPhone.Location = New System.Drawing.Point(206, 232)
+        Me.txtPhone.Name = "txtPhone"
+        Me.txtPhone.Size = New System.Drawing.Size(144, 20)
+        Me.txtPhone.TabIndex = 21
+        '
+        'txtDOB
+        '
+        Me.txtDOB.Location = New System.Drawing.Point(206, 257)
+        Me.txtDOB.Name = "txtDOB"
+        Me.txtDOB.Size = New System.Drawing.Size(144, 20)
+        Me.txtDOB.TabIndex = 22
+        '
+        'txtDOH
+        '
+        Me.txtDOH.Location = New System.Drawing.Point(206, 282)
+        Me.txtDOH.Name = "txtDOH"
+        Me.txtDOH.Size = New System.Drawing.Size(144, 20)
+        Me.txtDOH.TabIndex = 23
+        '
+        'txtDept
+        '
+        Me.txtDept.Location = New System.Drawing.Point(206, 307)
+        Me.txtDept.Name = "txtDept"
+        Me.txtDept.Size = New System.Drawing.Size(144, 20)
+        Me.txtDept.TabIndex = 24
+        '
+        'txtJob
+        '
+        Me.txtJob.Location = New System.Drawing.Point(206, 332)
+        Me.txtJob.Name = "txtJob"
+        Me.txtJob.Size = New System.Drawing.Size(144, 20)
+        Me.txtJob.TabIndex = 25
+        '
+        'Label14
+        '
+        Me.Label14.AutoSize = True
+        Me.Label14.Location = New System.Drawing.Point(86, 363)
+        Me.Label14.Name = "Label14"
+        Me.Label14.Size = New System.Drawing.Size(81, 13)
+        Me.Label14.TabIndex = 26
+        Me.Label14.Text = "Active Indicator"
+        '
+        'CheckBox1
+        '
+        Me.CheckBox1.AutoSize = True
+        Me.CheckBox1.Location = New System.Drawing.Point(206, 359)
+        Me.CheckBox1.Name = "CheckBox1"
+        Me.CheckBox1.Size = New System.Drawing.Size(15, 14)
+        Me.CheckBox1.TabIndex = 27
+        Me.CheckBox1.UseVisualStyleBackColor = True
+        '
+        'btnFirst
+        '
+        Me.btnFirst.Location = New System.Drawing.Point(12, 416)
+        Me.btnFirst.Name = "btnFirst"
+        Me.btnFirst.Size = New System.Drawing.Size(75, 23)
+        Me.btnFirst.TabIndex = 28
+        Me.btnFirst.Text = "First"
+        Me.btnFirst.UseVisualStyleBackColor = True
+        '
+        'btnPrior
+        '
+        Me.btnPrior.Location = New System.Drawing.Point(93, 416)
+        Me.btnPrior.Name = "btnPrior"
+        Me.btnPrior.Size = New System.Drawing.Size(75, 23)
+        Me.btnPrior.TabIndex = 29
+        Me.btnPrior.Text = "Prior"
+        Me.btnPrior.UseVisualStyleBackColor = True
+        '
+        'btnNext
+        '
+        Me.btnNext.Location = New System.Drawing.Point(174, 416)
+        Me.btnNext.Name = "btnNext"
+        Me.btnNext.Size = New System.Drawing.Size(75, 23)
+        Me.btnNext.TabIndex = 30
+        Me.btnNext.Text = "Next"
+        Me.btnNext.UseVisualStyleBackColor = True
+        '
+        'btnLast
+        '
+        Me.btnLast.Location = New System.Drawing.Point(255, 416)
+        Me.btnLast.Name = "btnLast"
+        Me.btnLast.Size = New System.Drawing.Size(75, 23)
+        Me.btnLast.TabIndex = 31
+        Me.btnLast.Text = "Last"
+        Me.btnLast.UseVisualStyleBackColor = True
+        '
+        'btnClose
+        '
+        Me.btnClose.Location = New System.Drawing.Point(336, 416)
+        Me.btnClose.Name = "btnClose"
+        Me.btnClose.Size = New System.Drawing.Size(75, 23)
+        Me.btnClose.TabIndex = 32
+        Me.btnClose.Text = "Close"
+        Me.btnClose.UseVisualStyleBackColor = True
+        '
+        'lblPosition
+        '
+        Me.lblPosition.AutoSize = True
+        Me.lblPosition.Location = New System.Drawing.Point(86, 389)
+        Me.lblPosition.Name = "lblPosition"
+        Me.lblPosition.Size = New System.Drawing.Size(45, 13)
+        Me.lblPosition.TabIndex = 33
+        Me.lblPosition.Text = "Label15"
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(425, 468)
+        Me.Controls.Add(Me.lblPosition)
+        Me.Controls.Add(Me.btnClose)
+        Me.Controls.Add(Me.btnLast)
+        Me.Controls.Add(Me.btnNext)
+        Me.Controls.Add(Me.btnPrior)
+        Me.Controls.Add(Me.btnFirst)
+        Me.Controls.Add(Me.CheckBox1)
+        Me.Controls.Add(Me.Label14)
+        Me.Controls.Add(Me.txtJob)
+        Me.Controls.Add(Me.txtDept)
+        Me.Controls.Add(Me.txtDOH)
+        Me.Controls.Add(Me.txtDOB)
+        Me.Controls.Add(Me.txtPhone)
+        Me.Controls.Add(Me.txtZIP)
+        Me.Controls.Add(Me.txtState)
+        Me.Controls.Add(Me.txtCity)
+        Me.Controls.Add(Me.txtAddress2)
+        Me.Controls.Add(Me.txtAddress1)
+        Me.Controls.Add(Me.txtLastName)
+        Me.Controls.Add(Me.txtFirstName)
+        Me.Controls.Add(Me.txtEmpID)
+        Me.Controls.Add(Me.Label13)
+        Me.Controls.Add(Me.Label12)
+        Me.Controls.Add(Me.Label11)
+        Me.Controls.Add(Me.Label10)
+        Me.Controls.Add(Me.Label9)
+        Me.Controls.Add(Me.Label8)
+        Me.Controls.Add(Me.Label7)
+        Me.Controls.Add(Me.Label6)
+        Me.Controls.Add(Me.Label5)
+        Me.Controls.Add(Me.Label4)
+        Me.Controls.Add(Me.Label3)
+        Me.Controls.Add(Me.Label2)
+        Me.Controls.Add(Me.Label1)
+        Me.Name = "Form1"
+        Me.Text = "Form1"
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+
+    Friend WithEvents Label1 As Label
+    Friend WithEvents Label2 As Label
+    Friend WithEvents Label3 As Label
+    Friend WithEvents Label4 As Label
+    Friend WithEvents Label5 As Label
+    Friend WithEvents Label6 As Label
+    Friend WithEvents Label7 As Label
+    Friend WithEvents Label8 As Label
+    Friend WithEvents Label9 As Label
+    Friend WithEvents Label10 As Label
+    Friend WithEvents Label11 As Label
+    Friend WithEvents Label12 As Label
+    Friend WithEvents Label13 As Label
+    Friend WithEvents txtEmpID As TextBox
+    Friend WithEvents txtFirstName As TextBox
+    Friend WithEvents txtLastName As TextBox
+    Friend WithEvents txtAddress1 As TextBox
+    Friend WithEvents txtAddress2 As TextBox
+    Friend WithEvents txtCity As TextBox
+    Friend WithEvents txtState As TextBox
+    Friend WithEvents txtZIP As TextBox
+    Friend WithEvents txtPhone As TextBox
+    Friend WithEvents txtDOB As TextBox
+    Friend WithEvents txtDOH As TextBox
+    Friend WithEvents txtDept As TextBox
+    Friend WithEvents txtJob As TextBox
+    Friend WithEvents Label14 As Label
+    Friend WithEvents CheckBox1 As CheckBox
+    Friend WithEvents btnFirst As Button
+    Friend WithEvents btnPrior As Button
+    Friend WithEvents btnNext As Button
+    Friend WithEvents btnLast As Button
+    Friend WithEvents btnClose As Button
+    Friend WithEvents lblPosition As Label
+End Class
---
+++ Database/Form1.vb
@@ -0,0 +1,72 @@
+﻿Public Class Form1
+    Private myDS As New DataSet
+    Private nRow As Integer
+
+    Private Sub btnClose_Click(sender As Object, e As EventArgs) Handles btnClose.Click
+        Me.Close()
+
+    End Sub
+
+    Private Sub Form1_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+        Dim myDA As New OleDb.OleDbDataAdapter
+        Dim myCommand As New OleDb.OleDbCommand
+        Dim myConnection As New OleDb.OleDbConnection
+
+        myConnection.ConnectionString = "Provider=Microsoft.ACE.OLEDB.12.0;Data Source=C:\Users\delus\Documents\Database1.accdb"
+        myCommand.CommandText = "SELECT Employees.DeptCode, Employees.DateHire, Employees.DateBirth, Employees.PhoneNumber, Employees.Zip, Employees.State, Employees.City, Employees.Address2, Employees.Address1, Employees.FirstName,  Employees.LastName, Employees.ID,Employees.Activeind, JobLookUp.[Desc], DeptLookUp.[Desc] AS Expr1 FROM ((DeptLookUp INNER JOIN Employees ON DeptLookUp.ID = Employees.DeptCode) INNER JOIN JobLookUp ON Employees.JobCode = JobLookUp.Code)"
+        myCommand.Connection = myConnection
+        myDA.SelectCommand = myCommand
+        myDA.Fill(myDS)
+
+        nRow = 0
+        LoadFormData()
+    End Sub
+
+    Private Sub LoadFormData()
+        txtEmpID.Text = myDS.Tables(0).Rows(nRow)("ID").ToString
+        txtFirstName.Text = myDS.Tables(0).Rows(nRow)("FirstName").ToString
+        txtLastName.Text = myDS.Tables(0).Rows(nRow)("LastName").ToString
+        txtAddress1.Text = myDS.Tables(0).Rows(nRow)("Address1").ToString
+        txtAddress2.Text = myDS.Tables(0).Rows(nRow)("Address2").ToString
+        txtCity.Text = myDS.Tables(0).Rows(nRow)("City").ToString
+        txtState.Text = myDS.Tables(0).Rows(nRow)("State").ToString
+        txtZIP.Text = myDS.Tables(0).Rows(nRow)("Zip").ToString
+        txtPhone.Text = myDS.Tables(0).Rows(nRow)("PhoneNumber").ToString
+        txtDOB.Text = myDS.Tables(0).Rows(nRow)("DateBirth").ToString
+        txtDOH.Text = myDS.Tables(0).Rows(nRow)("DateHire").ToString
+        txtDept.Text = myDS.Tables(0).Rows(nRow)("Expr1").ToString
+        txtJob.Text = myDS.Tables(0).Rows(nRow)("Desc").ToString
+        CheckBox1.Checked = myDS.Tables(0).Rows(nRow)("ActiveInd").ToString
+        lblPosition.Text = "Record " & nRow + 1 & " of " & myDS.Tables(0).Rows.Count
+
+    End Sub
+
+    Private Sub btnFirst_Click(sender As Object, e As EventArgs) Handles btnFirst.Click
+        nRow = 0
+        LoadFormData()
+    End Sub
+
+    Private Sub btnPrior_Click(sender As Object, e As EventArgs) Handles btnPrior.Click
+        nRow -= 1
+
+        If nRow < 0 Then
+            nRow = 0
+        End If
+        LoadFormData()
+    End Sub
+
+    Private Sub btnNext_Click(sender As Object, e As EventArgs) Handles btnNext.Click
+        nRow += 1
+
+        If nRow > myDS.Tables(0).Rows.Count - 1 Then
+            nRow = myDS.Tables(0).Rows.Count - 1
+        End If
+        LoadFormData()
+    End Sub
+
+    Private Sub btnLast_Click(sender As Object, e As EventArgs) Handles btnLast.Click
+        nRow = myDS.Tables(0).Rows.Count - 1
+        LoadFormData()
+
+    End Sub
+End Class
